fcadc45ca14d2856c75ede13484be39327af6050
konradroesler <konrad.rosler@gmail.com>
2024-03-13 15:32:49 +0100

    better none ls config

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -1,32 +1,31 @@
 return {
-	{
-		"nvimtools/none-ls.nvim",
-		config = function()
-			local null_ls = require("null-ls")
-			null_ls.setup({
-				sources = {},
+    {
+        "nvimtools/none-ls.nvim",
+        config = function()
+            local null_ls = require("null-ls")
+            null_ls.setup({
+                sources = {},
             })
-
-			vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
-		end,
-	},
-	{
-		"jay-babu/mason-null-ls.nvim",
-		event = { "BufReadPre", "BufNewFile" },
-		dependencies = {
-			"williamboman/mason.nvim",
-			"nvimtools/none-ls.nvim",
-		},
-		config = function()
-			local mason_null_ls = require("mason-null-ls")
+            vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
+        end,
+    },
+    {
+        "jay-babu/mason-null-ls.nvim",
+        event = { "BufReadPre", "BufNewFile" },
+        dependencies = {
+            "williamboman/mason.nvim",
+            "nvimtools/none-ls.nvim",
+        },
+        config = function()
+            local mason_null_ls = require("mason-null-ls")
             mason_null_ls.setup({
                 ensure_installed = {
-                    'stylua',
-                    'eslint_d',
-                    'prettierd',
+                    "stylua",
+                    "eslint_d",
+                    "prettierd",
                 },
                 automatic_installation = true,
             })
-		end,
-	},
+        end,
+    },
 }
