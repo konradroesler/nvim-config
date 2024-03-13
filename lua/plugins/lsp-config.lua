a6e6cfc8e134634f89f31876eb2fbf2ad06330bd
konradroesler <konrad.rosler@gmail.com>
2024-03-13 15:33:09 +0100

    added more lsp related keybindings

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -42,10 +42,14 @@ return {
 				capabilities = capabilities,
 			})
 
-			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-			vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
-			vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
-			vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
+			vim.keymap.set("n", "K", vim.lsp.buf.hover, {buffer=0})
+			vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {buffer=0})
+			vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {buffer=0})
+      vim.keymap.set("n", "<leader>gt", vim.lsp.buf.type_definition, {buffer=0})
+			vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {buffer=0})
+      vim.keymap.set("n", "<leader>gi", vim.lsp.buf.implementation, {buffer=0})
+      vim.keymap.set("n", "<leader>dn", vim.diagnostic.goto_next)
+      vim.keymap.set("n", "<leader>dp", vim.diagnostic.goto_prev)
 		end,
 	},
 }
