2f1c1a0207c13d4a5eb373cf5cc0fe76f041830b
konradroesler <konrad.rosler@gmail.com>
2024-03-16 21:15:42 +0100

    added ts-autotag to treesitter

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -3,11 +3,11 @@ return {
 		"nvim-treesitter/nvim-treesitter",
 		build = ":TSUpdate",
 		config = function()
-			local config = require("nvim-treesitter.configs")
-			config.setup({
+			require("nvim-treesitter.configs").setup({
 				auto_install = true,
 				highlight = { enable = true },
 				indent = { enable = true },
+        autotag = { enable = true },
 			})
 		end,
 	},
